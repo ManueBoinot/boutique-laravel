f2d2dcb6cbc000978dc5554832551e04c30f7fcc
TonySic <nanotsic@gmail.com>
2023-01-12 16:50:58 +0100

    finalisation Tony

--- resources/views/bandeau-promo.blade.php
+++ resources/views/bandeau-promo.blade.php
@@ -1,8 +1,15 @@
-<div class="text-center py-2"
-    style="background: rgb(255,225,64);
-background: radial-gradient(circle, rgba(252,235,145,1) 0%, rgba(255,225,64,1) 100%); color: black;">
-    <h2 class="text-uppercase">PROMOTION SPÉCIALE campagne_nom <span class="text-lowercase fst-italic">(du campagne_date_debut au
-            campagne_date_fin)</span></h2>
-    <h3>-campagne_reduction% sur une sélection de produits</h3>
+{{-- AFFICHAGE DU BANDEAU PROMO SI PROMO EN COURS SINON MASQUÉ --}}
+@if (isset($promo))
+    <div class="d-flex text-center justify-content-center gap-5 align-items-center p-2"
+        style="background: rgb(255,225,64);
+background: radial-gradient(circle, rgba(252,235,145,1) 0%, rgba(255,225,64,1) 100%); color: black; z-index: 1;">
 
-</div>
+        <h4>Du <span class="fs-3 fw-bold">{{ date('d-m-Y', strtotime($promo->date_debut)) }}</span> au
+            <span class="fs-3 fw-bold">{{ date('d-m-Y', strtotime($promo->date_fin)) }}</span>
+        </h4>
+        <h4>PROMOTION SPÉCIALE " <span class="text-uppercase fs-3 fw-bold">{{ $promo->nom }}</span> "</h4>
+        <h4><span class="fw-bold fs-3">-{{ $promo->reduction }}%</span> sur une sélection de produits !</h4>
+        <a href="/campagne" type="button" class="btn btn-warning fs-5 fw-bold">Voir la sélection</a>
+
+    </div>
+@endif
